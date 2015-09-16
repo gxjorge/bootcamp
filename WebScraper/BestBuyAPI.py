9a3099427c8882a4ea65d435e6584cef6049cfa7
marciokita <marcio_kita@yahoo.com.br>
2015-09-16 17:11:06 +0200

    Bestbuy

--- WebScraper/BestBuyAPI.py
+++ WebScraper/BestBuyAPI.py
@@ -1,7 +1,9 @@
 __author__ = 'Marcio'
+
 import WebScraper
 
-apiKey="ekv953kjxe8nknnrqsw6c2rv"
+
+#apiKey="ekv953kjxe8nknnrqsw6c2rv"
 
 ### ALL REFRIGERATORS (NEED LOOP IN PAGE)
 #http://api.bestbuy.com/v1/products(longDescription=Refrigerator*)?show=sku,name&pageSize=100&page=5&apiKey=ekv953kjxe8nknnrqsw6c2rv&format=json
@@ -10,8 +12,34 @@ apiKey="ekv953kjxe8nknnrqsw6c2rv"
 ###detail review (need a list of reviews)
 ##http://api.bestbuy.com/v1/reviews(sku%20in(9037473))?format=json&apiKey=ekv953kjxe8nknnrqsw6c2rv&show=id,sku,title,comment,rating
 
-url = "http://api.bestbuy.com/v1/products(longDescription=Refrigerator*)?show=sku,name,customerReviewAverage,customerTopRated,color,salePrice,customerReviewCount&pageSize=100&page=5&apiKey="+apiKey+"&format=json"
+#url = "http://api.bestbuy.com/v1/products(longDescription=Refrigerator*)?show=sku,name,customerReviewAverage,customerTopRated,color,salePrice,customerReviewCount&pageSize=100&page=5&apiKey="+apiKey+"&format=xml"
 #url = "http://api.walmartlabs.com/v1/feeds/items?apiKey={"+apiKey+"}&categoryId=3944"
 
-json = WebScraper.getSoup(url)
-print(json)
+#bbsoup = len(WebScraper.getSoup(url))
+
+
+#print(len(WebScraper.getSoup(url)))
+
+
+apiKey="ekv953kjxe8nknnrqsw6c2rv"
+page=1
+bbSoupList=[]
+productList=[]
+productDic={}
+
+url = "http://api.bestbuy.com/v1/products(longDescription=Refrigerator*)?show=sku,name,customerReviewAverage,customerTopRated,color,salePrice,customerReviewCount&pageSize=100&page="+str(page)+"&apiKey="+apiKey+"&format=xml"
+
+while len(getSoup(url).findAll("sku")) > 0:
+    bbSoupList.append(getSoup(url))
+    print(page, len(getSoup(url).findAll("sku")))
+    page += 1
+    url = "http://api.bestbuy.com/v1/products(longDescription=Refrigerator*)?show=sku,name,customerReviewAverage,customerTopRated,color,salePrice,customerReviewCount&pageSize=100&page="+str(page)+"&apiKey="+apiKey+"&format=xml"
+
+for soup in bbSoupList:
+    productList.extend(soup.findAll("product"))
+
+for product in productList:
+    productDic[product.find("sku").getText()] = product.find("sku").getText()
+
+
+
