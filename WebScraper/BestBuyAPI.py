e3da0f7cb3953ad596593e1dd36788ecba106368
marciokita <marcio_kita@yahoo.com.br>
2015-09-18 15:06:01 +0200

    Bestbuy - Almost Done

--- WebScraper/BestBuyAPI.py
+++ WebScraper/BestBuyAPI.py
@@ -1,45 +1,53 @@
-__author__ = 'Marcio'
-
+#import requests
+#import bs4
 import WebScraper
 
-
-#apiKey="ekv953kjxe8nknnrqsw6c2rv"
-
-### ALL REFRIGERATORS (NEED LOOP IN PAGE)
-#http://api.bestbuy.com/v1/products(longDescription=Refrigerator*)?show=sku,name&pageSize=100&page=5&apiKey=ekv953kjxe8nknnrqsw6c2rv&format=json
-#http://api.bestbuy.com/v1/products(longDescription=Refrigerator*)?show=sku,name,customerReviewAverage,customerTopRated,color,salePrice,customerReviewCount&pageSize=100&page=5&apiKey=ekv953kjxe8nknnrqsw6c2rv&format=json
-
-###detail review (need a list of reviews)
-##http://api.bestbuy.com/v1/reviews(sku%20in(9037473))?format=json&apiKey=ekv953kjxe8nknnrqsw6c2rv&show=id,sku,title,comment,rating
-
-#url = "http://api.bestbuy.com/v1/products(longDescription=Refrigerator*)?show=sku,name,customerReviewAverage,customerTopRated,color,salePrice,customerReviewCount&pageSize=100&page=5&apiKey="+apiKey+"&format=xml"
-#url = "http://api.walmartlabs.com/v1/feeds/items?apiKey={"+apiKey+"}&categoryId=3944"
-
-#bbsoup = len(WebScraper.getSoup(url))
-
-
-#print(len(WebScraper.getSoup(url)))
-
-
-apiKey="ekv953kjxe8nknnrqsw6c2rv"
-page=1
-bbSoupList=[]
-productList=[]
-productDic={}
-
-url = "http://api.bestbuy.com/v1/products(longDescription=Refrigerator*)?show=sku,name,customerReviewAverage,customerTopRated,color,salePrice,customerReviewCount&pageSize=100&page="+str(page)+"&apiKey="+apiKey+"&format=xml"
-
-while len(getSoup(url).findAll("sku")) > 0:
-    bbSoupList.append(getSoup(url))
-    print(page, len(getSoup(url).findAll("sku")))
-    page += 1
+def dataDicBB():
+    ##Variables
+    apiKey="ekv953kjxe8nknnrqsw6c2rv"
+    page=9 ### colocar 1 --- 9 test
+    bbSoupList=[]
+    productList=[]
+    productDic={}
     url = "http://api.bestbuy.com/v1/products(longDescription=Refrigerator*)?show=sku,name,customerReviewAverage,customerTopRated,color,salePrice,customerReviewCount&pageSize=100&page="+str(page)+"&apiKey="+apiKey+"&format=xml"
+    
+    while len(WebScraper.getSoup(url).findAll("sku")) > 0:
+        bbSoupList.append(WebScraper.getSoup(url))
+        #print(page, len(WebScraper.getSoup(url).findAll("sku")))
+        page += 1
+        url = "http://api.bestbuy.com/v1/products(longDescription=Refrigerator*)?show=sku,name,customerReviewAverage,customerTopRated,color,salePrice,customerReviewCount&pageSize=100&page="+str(page)+"&apiKey="+apiKey+"&format=xml"
+    
+    for soup in bbSoupList:
+        productList.extend(soup.findAll("product"))
+    
+    for product in productList:
+        #print(len(product))
+        
+        if product.find("color"):
+            color=product.find("color").getText()
+        else:
+            color="none"    
+        
+        productDic[product.find("sku").getText()] = product.find("sku").getText(),\
+        product.find("name").getText(),\
+        product.find("customerreviewaverage").getText(),\
+        color,\
+        product.find("saleprice").getText(),\
+        product.find("customerreviewcount").getText()
+    
+    reviewDic={}       
+    for key in productDic:
+        soupReview = WebScraper.getSoup("http://api.bestbuy.com/v1/reviews(sku%20in("+key+"))?format=xml&apiKey="+apiKey+"&show=id,sku,title,comment,rating")
+        reviewDic[key]=soupReview.findAll("comment")
+        #print(key)
+    
+    return productDic,reviewDic
+    
 
-for soup in bbSoupList:
-    productList.extend(soup.findAll("product"))
+   
+dataDicBB()
+   
+   
+    
 
-for product in productList:
-    productDic[product.find("sku").getText()] = product.find("sku").getText()
-
-
-
+    
