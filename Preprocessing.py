42f7428430fa45981f36317e3b27b05f76a9e69a
jgarciax <gxjorge@hotmail.com>
2015-10-12 15:09:52 +0200

    cleaning bestbuy

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,6 +1,7 @@
 __author__ = 'Jorge.GarciaXimenez'
 import pandas as pd
 import matplotlib.pyplot as plt
+import collections
 
 productsBestbuy = pd.read_excel("WebScraper/Export_Products.xlsx")
 #print(productsBestbuy.head())
@@ -9,12 +10,29 @@ reviewsBestbuy = pd.read_excel("WebScraper/Export_Reviews.xlsx")
 resultBestbuy = pd.merge(reviewsBestbuy, productsBestbuy, how='left', left_on="SKU", right_on="sku")
 #print(resultBestbuy.describe())
 #print(resultBestbuy.columns.values)
-print(resultBestbuy.describe())
+#print(resultBestbuy.describe())
 del resultBestbuy["Rest"]
-del resultBestbuy["Rest"]
+del resultBestbuy["Details: pricematch"]
+del resultBestbuy["Details: Ice Maker"]
+del resultBestbuy["description"]
+del resultBestbuy["sku"]
+del resultBestbuy["url"]
+del resultBestbuy["shippingCost"]
+#delete some columns because they are not completed
+del resultBestbuy["Details: Refrigerator Capacity"]
+#print(collections.Counter(resultBestbuy["Details: Refrigerator Capacity"]))
+del resultBestbuy["Details: Number Of Doors"]
+#print(collections.Counter(resultBestbuy["Details: Number Of Doors"]))
+resultBestbuy["Details: Freezer Capacity"] = resultBestbuy["Details: Freezer Capacity"].str.replace(" cubic feet","")
+resultBestbuy["Details: Height To Top Of Refrigerator"] = resultBestbuy["Details: Height To Top Of Refrigerator"].str.replace(" inches","")
 resultBestbuy.to_excel("Data/Data_Bestbuy.xlsx")
+print(resultBestbuy.columns.values)
 
-
+for col in resultBestbuy.columns.values:
+    if(col!='Comment'):
+        print(col)
+        print(collections.Counter(resultBestbuy[col]))
+"""
 productsWalmart = pd.read_excel("WebScraper/Export_Products_Walmart.xlsx")
 #print(productsWalmart.head())
 reviewsWalmart = pd.read_excel("WebScraper/Export_Reviews_Walmart.xlsx")
@@ -25,3 +43,4 @@ resultWalmart = pd.merge(reviewsWalmart, productsWalmart, how='left', left_on="i
 print(resultWalmart.columns.values)
 resultWalmart.to_excel("Data/Data_Walmart.xlsx")
 
+"""
